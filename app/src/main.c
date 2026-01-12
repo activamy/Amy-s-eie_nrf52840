d818b93009ec4d50afb0da7f6960892de15b1400
activamy <activamy38@gmail.com>
2026-01-11 21:41:02 -0700

    added includes to main.c and setted a few configuration values in prj.conf

--- app/src/main.c
+++ app/src/main.c
@@ -2,6 +2,21 @@
  * main.c
  */
 
+ #include <stdio.h>
+#include <string.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/settings/settings.h>
+#include <zephyr/sys/printk.h>
+
 #include <inttypes.h>
 
 #include <zephyr/kernel.h>
