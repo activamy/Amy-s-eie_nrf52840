be9ef5a84630009594a7bafdba36307f754c74d5
activamy <activamy38@gmail.com>
2026-03-13 09:38:41 -0600

    all functions written out, formats somewhat nicely

--- app/src/project_states.c
+++ app/src/project_states.c
@@ -33,13 +33,13 @@ static void review_entry(void* o);
 static enum smf_state_result review_run(void* o);
 static void review_exit(void* o);
 
-static void end_entry(void* o);
-static enum smf_state_result end_run(void* o);
-static void end_exit(void* o);
+// static void end_entry(void* o);
+// static enum smf_state_result end_run(void* o);
+// static void end_exit(void* o);
 
-static void delete_entry(void* o);
-static enum smf_state_result delete_run(void* o);
-static void delete_exit(void* o);
+static void restart_entry(void* o);
+static enum smf_state_result restart_run(void* o);
+static void restart_exit(void* o);
 
 static void final_entry(void* o);
 static enum smf_state_result final_run(void* o);
@@ -63,6 +63,8 @@ void led_setting(int led_num, char *state);
 */
 
 void turn_off_all_leds();
+void turn_on_all_leds();
+
 
 
 /*----------------------------------------------------------------
@@ -75,7 +77,7 @@ enum states_def {
     AMOUNT,
     RND_REVIEW,
     END_RND,
-    DELETE_OPTION,
+    RESTART_OPTIONS,
     FINISH_PROJECT
 };
 
@@ -92,7 +94,7 @@ enum stitches { // Keep adding on as needed
 };
 
 static const char *stitch_names[] = {
-    "",
+    "no",
     "MR",
     "Slip knot",
 
@@ -107,7 +109,7 @@ static const char *stitch_names[] = {
 typedef struct {
     enum stitches stitch;
     int amount;
-} the_stitch;
+} stitch_t;
 typedef struct {
   struct smf_ctx ctx;
 
@@ -117,7 +119,7 @@ typedef struct {
   int binary_amount[4]; // binary value from 0 to 9
   int decimal_conversion, decimal_amount;
   
-  the_stitch project_rnd[20][20]; // set limit for how many rounds now and how many can go in each list, edit later
+  stitch_t project_rnd[20][20]; // set limit for how many rounds now and how many can go in each list, edit later
 } project_states_objects_t;
 
 
@@ -131,14 +133,12 @@ static const struct smf_state states[] = {
     [STITCH_SELECTOR] = SMF_CREATE_STATE(selection_entry, selection_run, selection_exit, NULL, NULL),
     [AMOUNT] = SMF_CREATE_STATE(amount_entry, amount_run, amount_exit, NULL, NULL),
     [RND_REVIEW] = SMF_CREATE_STATE(review_entry, review_run, review_exit, NULL, NULL),
-    [END_RND] = SMF_CREATE_STATE(end_entry, end_run, end_exit, NULL, NULL),
-    [DELETE_OPTION] = SMF_CREATE_STATE(delete_entry, delete_run, delete_exit, NULL, NULL),
+    // [END_RND] = SMF_CREATE_STATE(end_entry, end_run, end_exit, NULL, NULL),
+    [RESTART_OPTIONS] = SMF_CREATE_STATE(restart_entry, restart_run, restart_exit, NULL, NULL),
     [FINISH_PROJECT] = SMF_CREATE_STATE(final_entry, final_run, final_exit, NULL, NULL)
 };
 
 static project_states_objects_t state_objects;
-static the_stitch stitch;
-
 
 /*----------------------------------------------------------------
  * State Definitions
@@ -163,7 +163,8 @@ static the_stitch stitch;
     memset(state_objects.binary_amount, 0, sizeof(state_objects.binary_amount));
     state_objects.round_number = 0;
     state_objects.stitch_number = 0;
-    printk("Project Begin: \n\nRound 1: ");
+    printk("--------------------\nProject Begin: \n\nRound 1: ");
+    LED_blink(LED0, LED_4HZ);
 }
 
  static enum smf_state_result start_run(void* o) {
@@ -184,12 +185,13 @@ static the_stitch stitch;
 
  static void start_exit(void* o) {
     state_objects.stitch_number++;
+    turn_off_all_leds();
  }
 
 
  // Sticth Selection
  static void selection_entry(void* o) {
-
+    LED_blink(LED1, LED_4HZ);
  } 
 
  static enum smf_state_result selection_run(void* o) {
@@ -221,13 +223,13 @@ static the_stitch stitch;
 }
 
  static void selection_exit(void* o) {
-
+    turn_off_all_leds();
  }
 
 
  // Amount of Stitches -------- Only use 0s and 1s
  static void amount_entry(void* o) {
-    state_objects.places = 0;
+    state_objects.places = 2;
     state_objects.count = 0;
     state_objects.decimal_conversion = 0;
     state_objects.decimal_amount = 0;
@@ -280,7 +282,7 @@ static the_stitch stitch;
         state_objects.decimal_conversion = binaryToDecimal(state_objects.binary_amount, state_objects.count);
         
         if (state_objects.places == 0) {
-            state_objects.decimal_amount = state_objects.decimal_conversion;
+            state_objects.decimal_amount += state_objects.decimal_conversion;
         }
         if (state_objects.places == 1) {
             state_objects.decimal_amount += state_objects.decimal_conversion * 10;
@@ -289,11 +291,11 @@ static the_stitch stitch;
             state_objects.decimal_amount += state_objects.decimal_conversion * 100;
         }
 
-        state_objects.places++;
+        state_objects.places--;
         state_objects.count = 0;
     }
 
-    if (state_objects.places == 3) {
+    if (state_objects.places == -1) {
         printk("%d ", state_objects.decimal_amount);
         state_objects.project_rnd[state_objects.round_number][state_objects.stitch_number].amount = state_objects.decimal_amount;
 
@@ -305,104 +307,147 @@ static the_stitch stitch;
 
  static void amount_exit(void* o) {
     turn_off_all_leds();
-    state_objects.stitch_number++;
  }
    
 
  // Rnd Review
  static void review_entry(void* o) {
     state_objects.first_appearance = 0;
+    LED_blink(LED2, LED_4HZ);
  }
  
  static enum smf_state_result review_run(void* o) {
     if (state_objects.first_appearance == 0) {
-        printk("The result for Round %d: ", state_objects.round_number + 1);
+        printk("\n\nThe result for Round %d: ", state_objects.round_number + 1);
         state_objects.first_appearance++;
 
-        for (int i = 0; i < 20; i++) {
-            if (state_objects.project_rnd[state_objects.round_number][i].stitch == NO_STITCHES) {
+        for (int i = 0; i < 20; i++) { 
+            if (strcmp(stitch_names[state_objects.project_rnd[state_objects.round_number][i].stitch], "no") == 0) {
                 break;
-        
-            printk("%s, ", stitch_names[state_objects.project_rnd[state_objects.round_number][i].stitch]);
             }
-        }
-        k_msleep(500);
 
-        printk("Select:\n
-            \t1 Continue to round %d\n
-            \t2) Finish project\n
-            \t3) Restart round\n
-            \t4) Restart entire project", 
-            state_objects.round_number + 2);
+            printk("%s %d, ", stitch_names[state_objects.project_rnd[state_objects.round_number][i].stitch], state_objects.project_rnd[state_objects.round_number][i].amount);
+        }
+
+        printk("\nSelect:\n\t1) Continue round %d\n\t2) Move to round %d\n\t3) Finish project\n\t4) Move to restart options\n\n", state_objects.round_number + 1, state_objects.round_number + 2);
+            //"Select:\n
+            // \t1) Continue to round %d\n
+            // \t2) Move to round %d\n
+            // \t3) Finish project\n 
+            // \t4)Move to restart options    }
     }
 
-    if (BTN_check_clear_pressed(BTN0)) { // Move to next round
+    if (BTN_check_clear_pressed(BTN0)) {   // Continue round
+        state_objects.stitch_number++;
+        printk("Round %d: ", state_objects.round_number + 1);
+
+        for (int i = 0; i < state_objects.stitch_number; i++) {
+            printk("%s %d, ", stitch_names[state_objects.project_rnd[state_objects.round_number][i].stitch], state_objects.project_rnd[state_objects.round_number][i].amount);
+        }
+        smf_set_state(SMF_CTX(&state_objects), &states[STITCH_SELECTOR]);
+    }
+
+    if (BTN_check_clear_pressed(BTN1)) {   // Move to next round
         state_objects.round_number++;
         state_objects.stitch_number = 0;
         printk("Round %d: ", state_objects.round_number + 1);
         smf_set_state(SMF_CTX(&state_objects), &states[STITCH_SELECTOR]);
     }
 
-    if (BTN_check_clear_pressed(BTN1)) { // End the project
+    if (BTN_check_clear_pressed(BTN2)) {   // End the project
         smf_set_state(SMF_CTX(&state_objects), &states[FINISH_PROJECT]);
     }
 
-    if (BTN_check_clear_pressed(BTN2)) { // Restart round
-        memset(state_objects.project_rnd[state_objects.round_number], 0, sizeof(state_objects.project_rnd[state_objects.round_number]));
-        state_objects.stitch_number = 0;
-        printk("Restarting...\nRound %d: ", state_objects.round_number + 1);
-        smf_set_state(SMF_CTX(&state_objects), &states[STITCH_SELECTOR]);
+    if (BTN_check_clear_pressed(BTN3)) {   // Move to Restart options
+        smf_set_state(SMF_CTX(&state_objects), &states[RESTART_OPTIONS]);
     }
 
     return SMF_EVENT_HANDLED;
-
- }
+}
 
  static void review_exit(void* o) {
-
+    turn_off_all_leds();
  }
   
 
 
- // Delete Option
- static void delete_entry(void* o) {
-
+ // Restart Option
+ static void restart_entry(void* o) {
+    state_objects.first_appearance = 0;
+    LED_blink(LED3, LED_4HZ);
+    printk("You have chosen restart options:\n");
  }
 
- static enum smf_state_result delete_run(void* o) {
+ static enum smf_state_result restart_run(void* o) {
+    if (state_objects.first_appearance == 0) {
+        printk("\t1) Restart round %d\n\t2) Restart project\n\t3) Cancel\n", state_objects.round_number + 1);
+
+        state_objects.first_appearance++;
+    }
+    // \t1) Restart round\n
+    // \t2) Restart project
+    // \t3) Cancel
+
+    if (BTN_check_clear_pressed(BTN0)) {
+        memset(state_objects.project_rnd[state_objects.round_number], 0, sizeof(state_objects.project_rnd[state_objects.round_number]));
+        state_objects.stitch_number = 0;
+        printk("\n----------\nRound %d: ", state_objects.round_number + 1);
+
+        smf_set_state(SMF_CTX(&state_objects), &states[STITCH_SELECTOR]);
+    }
+
+    if (BTN_check_clear_pressed(BTN1)) {
+        smf_set_state(SMF_CTX(&state_objects), &states[RND_1]);   //RND_1 already clears
+    }
+
     return SMF_EVENT_HANDLED;
 
  }
 
- static void delete_exit(void* o) {
-
+ static void restart_exit(void* o) {
+    turn_off_all_leds();
  }
  
 
  // Complete the Project
  static void final_entry(void* o) {
-    printk("\n------------------------------------------------\n
-        Printing Project... \n\n");
-    k_msleep(500);
+    state_objects.first_appearance = 0;
+    turn_on_all_leds();
  }
 
  static enum smf_state_result final_run(void* o) {
-    for (int j = 0; j < state_objects.round_number; j++) {
-        printk("Round %d: ", j + 1);
-        for (int k = 0; k < 20; k++) {
-            if (state_objects.project_rnd[j][k].stitch == NO_STITCHES) {
-                break;
+    if (state_objects.first_appearance == 0) {
+        int k;
+        printk("\n-----------------------\nPrinting Project... \n\n");
+        
+        for (int j = 0; j <= state_objects.round_number; j++) {
+            printk("Round %d: ", j + 1);
+            for (k = 0; k < 19; k++) {
+                if (state_objects.project_rnd[j][k+1].stitch == NO_STITCHES) {
+                    break;
+                }
+                printk("%s %d, ", stitch_names[state_objects.project_rnd[j][k].stitch], state_objects.project_rnd[j][k].amount);
             }
-            printk("%s %d, ", stitch_names[state_objects.project_rnd[j][k].stitch], state_objects.project_rnd[j][k].amount);
-        }
-    printk("\n");
-    }
-    return SMF_EVENT_HANDLED;
 
+            if (state_objects.project_rnd[j][k].stitch != NO_STITCHES) {
+                printk("%s %d\n", stitch_names[state_objects.project_rnd[j][k].stitch], state_objects.project_rnd[j][k].amount);
+            }
+        }
+
+        printk("\n----------End----------\n\n");
+        printk("Would you like to make another?\n\t1) Yes -> BTN0\n\n");
+        state_objects.first_appearance++;
+    }
+
+    if (BTN_check_clear_pressed(BTN0)) {
+        smf_set_state(SMF_CTX(&state_objects), &states[RND_1]);
+    }
+
+    return SMF_EVENT_HANDLED;
  }
 
  static void final_exit(void* o) {
-
+    turn_off_all_leds();
  }
 
 
@@ -427,7 +472,7 @@ int binaryToDecimal(int binary[], int length) {
 }
 
 
-
+// add more led functionalities here
 void led_setting(int led_num, char *state) {
 
     //Turn each on individually
@@ -443,7 +488,7 @@ void led_setting(int led_num, char *state) {
         LED_set(LED2, LED_ON);
     }
 
-    if (led_num == 3 && strcmp(state, "ON") == 0) {
+    if (led_num == 3) {
         LED_set(LED3, LED_ON);
     }
 
@@ -459,4 +504,11 @@ void turn_off_all_leds() {
     LED_set(LED1, LED_OFF);
     LED_set(LED2, LED_OFF);
     LED_set(LED3, LED_OFF);
+}
+
+void turn_on_all_leds() {
+    LED_set(LED0, LED_ON);
+    LED_set(LED1, LED_ON);
+    LED_set(LED2, LED_ON);
+    LED_set(LED3, LED_ON);
 }
