8861cabe011aca40a4692485ee97b4191eb21082
activamy <activamy38@gmail.com>
2026-08-26 17:34:27 -0600

    added button checking functions and print statements to check result

--- app/src/states.c
+++ app/src/states.c
@@ -3,6 +3,9 @@
 */
 
 #include <zephyr/smf.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
 
 #include "states.h"
 #include "BTN.h"
@@ -16,19 +19,14 @@
 static void start_entry(void* o);
 static enum smf_state_result start_run(void* o);
 
-static void st_inc_entry(void* o);
-static enum smf_state_result st_inc_run(void* o);
+static void btn_check_entry(void* o);
+static enum smf_state_result btn_check_run(void* o);
 
-// might not need
-// static void st_dec_entry(void* o);
-// static enum smf_state_result st_dec_run(void* o);
+static enum smf_state_result inc_run(void* o);
 
-static void rnd_inc_entry(void* o);
-static enum smf_state_result rnd_inc_run(void* o);
+static enum smf_state_result dec_run(void* o);
 
-static void rnd_dec_entry(void* o);
-static enum smf_state_result rnd_dec_run(void* o);
-
+static void reset_entry(void* o);
 static enum smf_state_result reset_run(void* o);
 
 
@@ -37,6 +35,7 @@ static enum smf_state_result reset_run(void* o);
  *-----------------------------*/
 void led_off();
 void led_initialize();
+void led_reset();
 
 void transmit_result(int st_count, int rnd_count);
 
@@ -45,18 +44,21 @@ void transmit_result(int st_count, int rnd_count);
  * Typedefs
  *-----------------------------*/
  enum states {
- 	START,              //everything is at 0 
- 	ST_INC,             // st++
- 	// ST_DEC,          // st--
- 	RND_INC,            // rnd++
- 	RND_DEC,            //rnd--
+ 	START,              // everything is at 0 
+ 	BTN_CHECK,          // trigger a flag based on btn
+ 	INC,                // inc
+ 	DEC,                // dec
  	RESET               // either st or rnd goes to 0
  };
 
 typedef struct {
     struct smf_ctx ctx;
     int st_count, rnd_count; 
-    int reset_flag;
+
+    int st, rnd;
+
+    int time_start;
+
 } cc_objects_t;
 
 
@@ -65,11 +67,10 @@ typedef struct {
  *-----------------------------*/
 static const struct smf_state states[] = {
     [START] = SMF_CREATE_STATE(start_entry, start_run, NULL, NULL, NULL),
-    [ST_INC] = SMF_CREATE_STATE(st_inc_entry, st_inc_run, NULL, NULL, NULL),
-    // [ST_DEC] = SMF_CREATE_STATE(st_dec_entry, st_dec_run, NULL, NULL, NULL),
-    [RND_INC] = SMF_CREATE_STATE(rnd_inc_entry, rnd_inc_run, NULL, NULL, NULL),
-    [RND_DEC] = SMF_CREATE_STATE(rnd_dec_entry, rnd_dec_run, NULL, NULL, NULL),
-    [RESET] = SMF_CREATE_STATE(NULL, reset_run, NULL, NULL, NULL)
+    [BTN_CHECK] = SMF_CREATE_STATE(btn_check_entry, btn_check_run, NULL, NULL, NULL),
+    [INC] = SMF_CREATE_STATE(NULL, inc_run, NULL, NULL, NULL),
+    [DEC] = SMF_CREATE_STATE(NULL, dec_run, NULL, NULL, NULL),
+    [RESET] = SMF_CREATE_STATE(reset_entry, reset_run, NULL, NULL, NULL)
 };
 
 static cc_objects_t cc_obj;
@@ -81,9 +82,15 @@ static cc_objects_t cc_obj;
 void crochet_counter_init() {
     cc_obj.st_count = 0;
     cc_obj.rnd_count = 0;
-    cc_obj.reset_flag = 0;
-    led_off();
-    smf_set_initial(SMF_CTX(&cc_obj), &states[START])
+
+    cc_obj.st = 0;
+    cc_obj.rnd = 0;
+
+    cc_obj.time_start = 0;
+
+    led_initialize();
+    printk("Starting!");
+    smf_set_initial(SMF_CTX(&cc_obj), &states[BTN_CHECK])
 }
 
 int crochet_counter_run() {
@@ -91,31 +98,93 @@ int crochet_counter_run() {
 }
 
 
-// Start
-static void start_entry(void* o) {
-    cc_obj.st_count = 0;
-    cc_obj.rnd_count = 0;
-    cc_obj.reset_flag = 0;
+// ensure led is off
+static void btn_check_entry(void* o) {
+    led_off();
+    cc_obj.time_start = 0;
 
-    led_initialize();
+    cc_obj.st = 0;
+    cc_obj.rnd = 0;
 }
 
-static enum smf_state_result start_run(void*o) {
-    if (BTN_check_clear_pressed(BTN0)) {
-        smf_set_state(SMF_CTX(&cc_obj), &states[ST_INC]);
+static enum smf_state_result btn_check_run(void* o) {
+    if (BTN_is_pressed(BTN0)) {                         // working with the stitch
+        cc_obj.st = 1;
+        if (cc_obj.time_start == 0) {
+            cc_obj.time_start = k_uptime_get();         
+        }
     }
-    else if (BTN_check_clear_pressed(BTN1)) {
-        smf_set_state(SMF_CTX(&cc_obj), &states[RND_INC]);
+    else if (BTN_is_pressed(BTN1)) {                   // working with the round
+        cc_obj.rnd = 1;
+        if (cc_obj.time_start == 0) {
+            cc_obj.time_start = k_uptime_get();
+        }
+    }
+
+    //check whether the intention is an inc, dec, or reset to zero
+    if ((!BTN_is_pressed(BTN0) || !BTN_is_pressed(BTN1)) && (cc_obj.time_start != 0)) {
+        if ((k_uptime_get() - cc_obj.time_start) >= 4000) {
+            smf_set_state(SMF_CTX(&cc_obj), &states[RESET]);
+        }
+        else if ((k_uptime_get() - cc_obj.time_start >= 1500) && ((k_uptime_get() - cc_obj.time_start) < 4000)) {
+            smf_set_state(SMF_CTX(&cc_obj), &states[DEC]);
+        }
+        else ((k_uptime_get() - cc_obj.time_start >= 0) && ((k_uptime_get() - cc_obj.time_start) < 1500)) {
+            smf_set_state(SMF_CTX(&cc_obj), &states[INC]);
+        }
     }
 
     return SMF_EVENT_HANDLED;
 }
 
 
-// Increase Stitch
-static void st_inc_entry(void* o) {
-    led_off();
-    cc_obj.st_count++;
+// INC
+static enum smf_state_result inc_run(void* o) {
+    if (cc_obj.st == 1) {
+        cc_obj.st_count++;
+        printk("st = %d", cc_obj.st_count);
+    }
+    else if (cc_obj.rnd == 1) {
+        cc_obj.rnd_count++;
+        printk("rnd = %d", cc_obj.rnd_count);
+    }
+    smf_set_state(SMF_CTX(&cc_obj), &states[BTN_CHECK]);
+    return SMF_EVENT_HANDLED;
+}
+
+
+// DEC
+static enum smf_state_result dec_run(void* o) {
+    if (cc_obj.st == 1) {
+        cc_obj.st_count--;
+        printk("st = %d", cc_obj.st_count);
+    }
+    else if (cc_obj.rnd == 1) {
+        cc_obj.rnd_count--;
+        printk("rnd = %d", cc_obj.rnd_count);
+    }
+    smf_set_state(SMF_CTX(&cc_obj), &states[BTN_CHECK]);
+    return SMF_EVENT_HANDLED;
+}
+
+
+// RESET
+static void reset_entry(void*o) {  
+    led_reset();
+}
+
+static enum smf_state_result reset_run(void* o) {
+    if (cc_obj.st == 1) {
+        cc_obj.st_count = 0;
+        printk("st cleared");
+    }
+    else if (cc_obj.rnd == 1) {
+        cc_obj.rnd_count = 0;
+        printk("rnd cleared!");
+
+    }
+    smf_set_state(SMF_CTX(&cc_obj), &states[BTN_CHECK]);
+    return SMF_EVENT_HANDLED;
 }
 
 
@@ -132,11 +201,19 @@ void led_off() {
 
 void led_initialize() {
     LED_set(LED0, 1);
-    kmsleep(100);
+    k_msleep(100);
     LED_set(LED1, 1);
-    kmsleep(100);
+    k_msleep(100);
     LED_set(LED2, 1);
-    kmsleep(100);
+    k_msleep(100);
+    LED_set(LED3, 1);
 }
 
-void transmit_result(int st_count, int rnd_count)
+void led_reset() {
+    LED_set(LED0, 1);
+    LED_set(LED1, 1);
+    LED_set(LED2, 1);
+    LED_set(LED3, 1);
+}
+
+// void transmit_result(int st_count, int rnd_count)
